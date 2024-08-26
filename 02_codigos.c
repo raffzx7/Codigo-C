d637445d4588540673fc5f020f0c2fe0e6e23bcc
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2024-08-26 08:25:22 -0300

    calcular a média de 4 notas dos alunos

--- 02_codigos.c
+++ 02_codigos.c
@@ -3,21 +3,21 @@
 int main() {
     float nota1, nota2, nota3, nota4, media;
 
-    printf("Digite a primeira nota: ");
+    printf("insira a primeira nota:\n");
     scanf("%f", &nota1);
 
-    printf("Digite a segunda nota: ");
+    printf("insira a segunda nota:\n");
     scanf("%f", &nota2);
 
-    printf("Digite a terceira nota: ");
+    printf("insira a terceira nota:\n");
     scanf("%f", &nota3);
 
-    printf("Digite a quarta nota: ");
+    printf("insira a quarta nota:\n");
     scanf("%f", &nota4);
 
     media = (nota1 + nota2 + nota3 + nota4) / 4;
 
-    printf("A média das quatro notas é: %.2f\n", media);
+    printf("a média das quatro notas é: %.2f\n", media);
 
     return 0;
 }
