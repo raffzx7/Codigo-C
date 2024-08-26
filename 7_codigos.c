aaa7cb3f59d900bfc84b02b6689f45e613c29aef
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2024-08-26 08:29:43 -0300

    cobrar 45 reais por quillo

--- 7_codigos.c
+++ 7_codigos.c
@@ -1,12 +1,10 @@
 include <stdio.h>
 
 int main() {
-
     float peso_prato, valor_a_pagar;
     const float preco_por_quilo = 45.00;
 
-  
-    printf("Digite o peso do prato em quilos: ");
+    printf("inisra o peso do prato em quilos: ");
     scanf("%f", &peso_prato);
 
     valor_a_pagar = peso_prato * preco_por_quilo;
@@ -14,5 +12,4 @@ int main() {
     printf("O valor a pagar é: R$%.2f\n", valor_a_pagar);
 
     return 0;
-
 }
