439b05ae0dd43ec51848f80cdd07554c5b427196
Rafael Silas Silva De Souza <rafazouza588@gmail.com>
2024-08-26 08:37:38 -0300

    calcular divisão entre dois números

--- 05_codigos.c
+++ 05_codigos.c
@@ -3,16 +3,16 @@
 int main() {
     float valor1, valor2, divisao;
 
-    printf("inisra o primeiro valor: ");
+    printf("inisra o primeiro valor:\n");
     scanf("%f", &valor1);
 
-    printf("inisra o segundo valor: ");
+    printf("inisra o segundo valor:\n");
     scanf("%f", &valor2);
 
     if (valor2 != 0) {
 
         divisao = valor1 / valor2;
-        printf("A divisão do primeiro valor pelo segundo é: %.2f\n", divisao);
+        printf("a divisão do primeiro valor pelo segundo é: %.2f\n", divisao);
 
     } else {
         printf("erro: divisão por zero não é permitida.\n");
